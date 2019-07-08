11a663def544f55532ef03bbb55a44630235bda2
drsjb80 <drsjb80@gmail.com>
2019-07-07 19:20:56 -0600

    Refactored and more tests added.

--- src/test/java/edu/msudenver/cs/JPOFPTest.java
+++ src/test/java/edu/msudenver/cs/JPOFPTest.java
@@ -6,37 +6,44 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.prefs.BackingStoreException;
 import java.util.prefs.InvalidPreferencesFormatException;
 import java.util.prefs.Preferences;
 
 import static org.junit.Assert.*;
 
 class JdnssArgs {
-    private int backlog;
+    String addresses;
+    int backlog;
+    long logback;
+    float shortPi;
+    double longPi;
+    boolean debug;
 }
 
 public class JPOFPTest {
     @Test
     public void simple() {
-        try {
-            InputStream is = new BufferedInputStream(new FileInputStream("jdnss.xml"));
+        try (InputStream is = new BufferedInputStream(new FileInputStream("jdnss.xml"))) {
             Preferences.importPreferences(is);
-            is.close();
         } catch (IOException | InvalidPreferencesFormatException e) {
             e.printStackTrace();
             return;
         }
 
+        JdnssArgs jdnssArgs = new JdnssArgs();
         Preferences prefs = Preferences.userRoot().node("edu/msudenver/cs/jdnss");
-        try {
-            for (String s : prefs.keys()) {
-                System.out.println(s);
-            }
-        } catch (BackingStoreException e) {
-            e.printStackTrace();
-        }
-
-        new JPOFP(new JdnssArgs(), prefs);
+        assertNull(jdnssArgs.addresses);
+        assertEquals(0, jdnssArgs.backlog);
+        assertEquals(0L, jdnssArgs.logback);
+        assertEquals(0.0, jdnssArgs.shortPi, 0);
+        assertEquals(0.0D, jdnssArgs.longPi, 0);
+        assertFalse(jdnssArgs.debug);
+        JPOFP.doJPOFP(jdnssArgs, prefs);
+        assertEquals("TLS@0.0.0.0@853", jdnssArgs.addresses);
+        assertEquals(5, jdnssArgs.backlog);
+        assertEquals(5, jdnssArgs.logback);
+        assertEquals(3.14, jdnssArgs.shortPi, 0.001);
+        assertEquals(3.141592, jdnssArgs.longPi, 0.000000001);
+        assertTrue(jdnssArgs.debug);
     }
 }
